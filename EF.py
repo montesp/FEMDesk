1a10c2d937c69fe176346d7c772015c827841e29
montesp <pavelmontes22@gmail.com>
2022-07-06 22:50:19 -0600

    Cambios en la interfaz

--- EF.py
+++ EF.py
@@ -1,16 +1,12 @@
-#-*- coding: utf-8 -*-
 """
 Created on Wed May 11 13:39:55 2022
 
-@author: ruben.castaneda
+@author:ruben.castaneda,
+        Pavel Montes,
+        Armando Teran
 """
 
 #-*- coding: utf-8 -*-
-"""
-Created on Tue May 10 14:30:09 2022
-
-@author: ruben.castaneda
-"""
 
 import os, sys
 import imagen_rc
@@ -21,25 +17,22 @@ from PyQt5.uic import loadUi
 from PyQt5 import QtWidgets
 
 app = None
-       
-class EditorWindow(QMainWindow):
-    """MainWindow-klass som hanterar vårt huvudfönster"""
 
+class EditorWindow(QMainWindow):
     def __init__(self):
         super(QMainWindow, self).__init__()
         self.app = app
         root = os.path.dirname(os.path.realpath(__file__))
         loadUi(os.path.join(root, 'Interfaz.ui'), self)
-            
+
 def init_app():
     app = QApplication.instance()
 
     if app is None:
-        print("No QApplication instance found. Creating one.")
+        # print("No QApplication instance found. Creating one.")
         # if it does not exist then a QApplication is created
         app = QApplication(sys.argv)
-    else:
-        print("QApplication instance found.")
+
 
     return app
 
