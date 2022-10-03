560b8490296250ccac89734404c3b83c43adf090
Pavel Montes <pavelmontes22@gmail.com>
2022-10-03 10:17:27 -0600

    Cada figuras ya guarda la linea

--- Modules/SectionTabs/Conditions.py
+++ Modules/SectionTabs/Conditions.py
@@ -10,18 +10,32 @@ class Conditions():
             tbox.insertItem(0, array[1], str(comb.currentText()))
 
     def reloadEdges(canvas, listWid):
-        edges = canvas.getEdges()
-        print('here')
-        print(edges)
+        polygons , edges = canvas.getAll()
+        # print(polygons)
+        # print(edges)
         listOfEdges = []
+        listOfPolys = []
 
         if listWid.count() != 0:
             listWid.clear()
         
-        for i in range(len(edges)):
-            text = "linea " + str(i+1)
-            listOfEdges.append([edges[i], text ])
-            listWid.addItem(text)
+        for i in range(len(polygons)):
+            # print("Figura " + str(i+1))
+            polygon = "Figura " + str(i+1)
+            listOfPolys.append({'polygon':polygons[i], 'text':polygon })
+            # listWid.addItem(polygon)
+
+        for poly in listOfPolys:
+            # print(poly['text'])
+            listWid.addItem(poly['text'])
+            for i in range(len(poly['polygon'][0])):
+                # print(i)
+                text = "linea " + str(i+1)
+            # listOfEdges.append([edges[i], text ])
+                listWid.addItem(text)
+
+        # print(listOfPolys)
     
     def currentElementSelectListWidgets(element):
-        print(element)
+        value = element.text()
+        print(value)
