0c2a945ab9c1e2992c2a0961d5c7390d8498a965
Armando Teran <Armando55441@hotmail.com>
2022-10-13 20:00:28 -0600

    actualizacion de combobox terminado

--- Modules/Dictionary/DMatrix.py
+++ Modules/Dictionary/DMatrix.py
@@ -13,3 +13,14 @@ diffusionMatrix = {
     "inputMode" : 0,
 }
 
+coordinates = {
+    "coordinateDiffusion" : [0] * 2,
+    "coordinateAbsorption" : [0] * 2,
+    "coordinateSource" : [0],
+    "coordinateMass" : [0] * 2,
+    "coordinateDamMass" : [0] * 2,
+    "coordinateCFlux" : [0] * 2,
+    "coordinateConvection" : [0] * 2,
+    "coordinateCSource" : [0]
+}
+
