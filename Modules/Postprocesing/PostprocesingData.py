4ecc80c92506bbb228ef490d3d028625d81f1200
Pavel Montes <pavelmontes22@gmail.com>
2022-11-26 14:58:52 -0600

    obtener matrix beta avance

--- Modules/Postprocesing/PostprocesingData.py
+++ Modules/Postprocesing/PostprocesingData.py
@@ -2,11 +2,11 @@ class PostprocesingData:
     def __init__(self) -> None:
         pass
     
-
     def getTypeConditions(self, win):
         sidesData = win.conditions.sidesData
         typeConditions = []
         typeConditonsBooleans = []
+        
 
         for sideData in sidesData:
             if sideData['typeCondition'] == "Temperature":
@@ -16,8 +16,28 @@ class PostprocesingData:
             typeConditions.append(sideData['typeCondition'])
 
 
+
+
         print(typeConditions)
         print(typeConditonsBooleans)
+        return typeConditonsBooleans, sidesData
 
+    def getMatrizBeta(self, win):
+        conditions = self.getTypeConditions(win)
+        valuesCFMatriz = []
+        matrixBeta = []
+
+
+        for condition in conditions:
+            if condition:
+                valuesCFMatriz.append(0)
+            else:
+                pass
+                # valuesCFMatriz.append() # Poner cf
+            matrixBeta.append(0)
+
+    def getDensityHeatCapacity():
+        pass 
+    
     def infoAdd():
         pass
