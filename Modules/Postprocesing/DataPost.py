75b6a15c6def3d636e50d5f1cd3b449b1007511f
Armando Teran <Armando55441@hotmail.com>
2022-11-27 00:15:45 -0700

    Conversion de Datos CF 1ra parte

--- Modules/Postprocesing/DataPost.py
+++ Modules/Postprocesing/DataPost.py
@@ -2,13 +2,30 @@ import numpy as np
 
 
 class DataPost():
-    ConditionsCF = []
+    ConditionsCF = None
 
     def recieveTypeConditions(ConditionsCFList, dataPost):
-        ConditionsCF = np.array(ConditionsCFList)
+        start = 0
+        DataPost.ConditionsCF = np.empty([len(ConditionsCFList) * len(ConditionsCFList[0]), 1] , dtype=np.object)
+        for i in range(len(ConditionsCFList[0])):
+            DataPost.ConditionsCF[start + i][0] = np.array(ConditionsCFList[0][i], dtype=np.int8)
+        start += len(ConditionsCFList[0])
+        for i in range(len(ConditionsCFList[1])):
+            DataPost.ConditionsCF[start + i][0] = np.array(ConditionsCFList[1][i], dtype=np.float64)
+        start += len(ConditionsCFList[1])
+        for i in range(len(ConditionsCFList[2])):
+            DataPost.ConditionsCF[start + i][0] = np.array(ConditionsCFList[2][i], dtype=np.int8)
+
+        DataPost.ConditionsCF = np.asarray(DataPost.ConditionsCF)
+        #ConditionsCF = np.array(ConditionsCFList[0], ConditionsCFList[1], ConditionsCFList[2])
+        '''for i in range(ConditionsCF.shape[0]):
+            for j in range(ConditionsCF.shape[1]):
+                ConditionsCF[i][j] = np.array([ConditionsCF[i][j]])'''
+
+        #np.reshape(ConditionsCF, (ConditionsCF.shape[0] * 3, 1))
         print('Postprocesado de Conditions Recibido')
-        print(ConditionsCF)
-        dataPost.append(ConditionsCF)
+        print(DataPost.ConditionsCF)
+        dataPost.append(DataPost.ConditionsCF)
         
 
     
